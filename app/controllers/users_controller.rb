8ea1a45aa119a1c111682299e0b0c7870e206a4c
SterlingVu <sterling.vu.goldenowl@gmail.com>
2020-01-06 21:41:49 +0700

    Refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = 'Welcome to the Sample App!'
-      redirect_to @user
+      redirect_to @user, notice: 'Welcome to the Sample App!'
     else
       render 'new'
     end
@@ -26,6 +25,11 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 end
