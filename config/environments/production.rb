c4afa913457d19eb12de65cb03a888b96a8a7cf3
SterlingVu <sterling.vu.goldenowl@gmail.com>
2020-01-07 17:32:47 +0700

    added default_url_options.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,7 @@ Rails.application.configure do
   #   :user_name      => ENV['GMAIL_USERNAME'],
   #   :password       => ENV['GMAIL_PASSWORD']
   # }
+  config.action_mailer.default_url_options = { host: 'secure-thicket-00563.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
